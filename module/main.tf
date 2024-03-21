026daf928fbaebf6bba09ae1ce522ff67c208d9a
SVR Kalluru <raghuk.vit@gmail.com>
2024-03-21 07:21:24 +0530

    Enabling Spot

--- module/main.tf
+++ module/main.tf
@@ -4,8 +4,13 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = [data.aws_security_group.selected.id]
   iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
 
+  spot_options {
+    instance_interruption_behavior = "stop"
+    spot_instance_type             = "persistent"
+  }
+
   tags = {
-    Name    = var.tool_name
+    Name = var.tool_name
   }
 }
 
